880e7b067e335d0ce034eed8ae783f94fc3dd75f
xian@oryza <icos.atropa@gmail.com>
2014-09-03 01:12:47 -0600

    Cleaned up user defined functions in userFun.cpp, added macros and examples of mapply.  Added tests.  Segfault for sampling large number of x??

--- inst/tests/userFun.cpp
+++ inst/tests/userFun.cpp
@@ -5,49 +5,74 @@ using namespace Rcpp;
 typedef void (*funcPtr)(arma::vec& x);
 typedef void (*funcPtrM)(arma::vec& x, double y);
 
+// Bodies of user-accessible functions 
+//
+#define RAGGED_SAPPLY(workerFunName) return(XPtr<funcPtr>(new funcPtr(&workerFunName)))
+#define RAGGED_MAPPLY(workerFunName) return(XPtr<funcPtrM>(new funcPtrM(&workerFunName)))
+
 // worker function to modify x in-place
-void myfunDefn(arma::vec& x) {
+void workerTimes10(arma::vec& x) {
     x = 10*x;
 }
 // followed by exported function to return external pointer
 // [[Rcpp::export]]
-XPtr<funcPtr> myfun_times10() {
-    return(XPtr<funcPtr>(new funcPtr(&myfunDefn)));
+XPtr<funcPtr> userFunTimes10() {
+    RAGGED_SAPPLY(workerTimes10);
+    //return(XPtr<funcPtr>(new funcPtr(&workerFunTimes10)));
 }
 
 
 // [[Rcpp::export]]
-NumericVector myfun_times10R(NumericVector& x) {
+NumericVector userFunTimes10R(NumericVector& x) {
     x = 10*x;
     return x;
 }
 
-void myfun1Defn(arma::vec& x) {    
+void workerSum(arma::vec& x) {
     x = arma::sum(x);
 }
 
 // [[Rcpp::export]]
-XPtr<funcPtr> myfun1_sum() {
-    return(XPtr<funcPtr>(new funcPtr(&myfun1Defn)));
+XPtr<funcPtr> userFunSum() {
+    RAGGED_SAPPLY(workerSum);
 }
 
-void defnSample(arma::vec& x) {    
-    RNGScope scope;
+void workerSample(arma::vec& x) {    
+    //RNGScope scope;
     x = RcppArmadillo::sample(x, x.size(), true);
 }
-
 // [[Rcpp::export]]
-XPtr<funcPtr> myfun_sample() {
-    return(XPtr<funcPtr>(new funcPtr(&defnSample)));
+XPtr<funcPtr> userFunSample() {
+    RAGGED_SAPPLY(workerSample);
 }
 
+void workerSampleBig(arma::vec& x) {    
+    //RNGScope scope;
+    x = RcppArmadillo::sample(x, 1e7, true);
+}
+// [[Rcpp::export]]
+XPtr<funcPtr> userFunSampleBig() {
+    RAGGED_SAPPLY(workerSampleBig);
+}
 
 // grow x
-void defnSample1(arma::vec& x) {    
-    RNGScope scope;
+void workerSample1(arma::vec& x) {
+    //RNGScope scope;
     x = RcppArmadillo::sample(x, x.size()+1, true);
 }
 // [[Rcpp::export]]
-XPtr<funcPtr> myfun_sample1() {
-    return(XPtr<funcPtr>(new funcPtr(&defnSample1)));
+XPtr<funcPtr> userFunSample1() {
+    RAGGED_SAPPLY(workerSample1);
+}
+
+// mapply version of sample
+void workerSampleM(arma::vec& x, double y) {
+    //RNGScope scope;
+    int iy = y;
+    x = RcppArmadillo::sample(x, iy, true);
+}
+// NOTE - return value of userFun has changed!
+// [[Rcpp::export]]
+XPtr<funcPtrM> userFunSampleM() {
+    RAGGED_MAPPLY(workerSampleM);
 }
